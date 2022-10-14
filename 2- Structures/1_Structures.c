10474affc888b8948fb33928732d2d8590752e2a
Raj Randive <randiveraj96@gmail.com>
2022-10-14 23:35:56 +0530

    Passed in first trial

---
+++ 2- Structures/1_Structures.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+
+struct Employee{
+    int emp_id;
+    char name[50];
+    int salary;
+};
+
+int main()
+{
+    struct Employee e;
+    e.emp_id = 1001;
+    e.salary = 100000;
+    // e.name = "Raj";    this will give us an error
+    strcpy(e.name, "Raj");
+
+    printf("\nThe Employee id is: %d", e.emp_id);
+    printf("\nThe Employee Name is: %s", e.name);
+    printf("\nThe Employee Salary is: %d", e.salary);
+
+
+    return 0;
+}
